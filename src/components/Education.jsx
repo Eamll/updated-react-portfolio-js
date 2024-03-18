e89d89d5418ef84b5239772084af5c3393b33681
Eamll <alexis.0804yes@gmail.com>
2024-03-18 00:07:21 -0400

    [REFACTOR] Add @mui/lab package and refactor Education component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,17 +1,10 @@
 import { Typography, Box, Container, Grid } from "@mui/material";
 import 'animate.css';
-import Timeline from "./Timeline";
+import TimelineCard from "./Timeline";
 
 
 
 const Education = () => {
-    const events = [
-        { title: 'Started School', description: 'I started school in 2000.' },
-        { title: 'Graduated High School', description: 'I graduated high school in 2012.' },
-        { title: 'Started University', description: 'I started university in 2013.' },
-        { title: 'Graduated University', description: 'I graduated university in 2017.' },
-        // Add more events here...
-    ];
     return (
         <>
             <Grid
@@ -23,18 +16,19 @@ const Education = () => {
             >
                 <Container>
                     <Grid container spacing={2}>
-                        <Grid item xs={6}>
+                        <Grid item >
                             <Box textAlign="left">
                                 <Typography variant="h4" component="h1" gutterBottom>
                                     Education & Experience
                                 </Typography>
-                                <Timeline events={events}></Timeline>
+                                <TimelineCard />
                             </Box>
                         </Grid>
 
                     </Grid>
                 </Container>
             </Grid>
+
         </>
     );
 }
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,90 +1,144 @@
-import PropTypes from "prop-types"
-import { useState, useRef } from 'react';
-import { Card, CardContent, Typography } from '@mui/material';
+import { Box, Typography, Grid, Modal } from '@mui/material';
+import { useState } from 'react';
 
-const timelineStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflowX: 'scroll',
-    gap: '20px',
-    padding: '20px 0',
-    scrollbarWidth: 'none', // For Firefox
-    msOverflowStyle: 'none', // For Internet Explorer and Edge
-};
+function TimelineCard() {
+    const contenttop = [
+        {
+            id: 1,
+            year: '2019 - 2023',
+            title: 'Licenciatura en Ingeniería de Sistemas',
+            office: 'UTEPSA',
+            text: '',
+        },
+        {
+            id: 2,
+            year: '2012 - 2016',
+            title: 'Inglés Nivel Avanzado Programa de Estudios',
+            office: 'CBA (Centro Boliviano Americano)',
+            text: '',
+        },
+        {
+            id: 3,
+            year: '11/2016',
+            title: 'TOEFL ITP (Test of English as a Foreign Language)',
+            office: 'CBA (Centro Boliviano Americano)',
+            text: '',
+        },
+        {
+            id: 4,
+            year: '09/2016',
+            title: 'TOEIC (Test of English for International Communication)',
+            office: 'CBA (Centro Boliviano Americano)',
+            text: '',
+        },
+        {
+            id: 5,
+            year: '2018',
+            title: 'Bachiller en Humanidades',
+            office: 'Colegio Boliviano Americano',
+            text: '',
+        },
+        {
+            id: 6,
+            year: '10/2023 - 12/2023',
+            title: 'Prácticas como Desarrollador Web',
+            office: 'AssureSoft, Santa Cruz de la Sierra',
+            text: 'Construir interfaces de aplicaciones web front-end utilizando Angular y TypeScript para lógica del lado del cliente y arquitectura basada en componentes. Desarrollar API de back-end robustas y escalables con Node.js, Express y base de datos MySQL para proporcionar datos al front-end. Desarrollar código bien diseñado, probado y documentado siguiendo las mejores prácticas para el desarrollo de software. Colaborar con equipos interfuncionales utilizando la metodología Scrum.',
+        },
+        {
+            id: 7,
+            year: '08/2023 - 10/2023',
+            title: 'Prácticas como Desarrollador Web',
+            office: 'Biopetrol EE.SS., Santa Cruz de la Sierra',
+            text: 'Construir sitios web y aplicaciones web receptivas para clientes utilizando PHP, CodeIgniter y Flutter con base de datos SQL Server, PostgreSQL y MySQL. Trabajar en estrecha colaboración con los clientes para comprender los requisitos del proyecto y diseñar soluciones personalizadas. Participar en todas las etapas del desarrollo, incluida la planificación, implementación y despliegue.',
+        },
+        {
+            id: 8,
+            year: '06/2020 - 05/2023',
+            title: 'Desarrollador Tecnológico',
+            office: 'Firma de Consultoría Contable y Tributaria "Solución"',
+            text: 'Desarrollar scripts de automatización y macros utilizando Visual Basic para agilizar los procesos y flujos de trabajo empresariales. Programar soluciones para minimizar la entrada manual de datos y maximizar la productividad del equipo de contabilidad.',
+        },
+    ];
 
-const timelineItemStyle = {
-    flex: '0 0 auto',
-};
+    // const [expandedText, setExpandedText] = useState({});
 
-const TimelineItem = ({ title, children }) => (
-    <Card style={timelineItemStyle}>
-        <CardContent>
-            <Typography variant="h6" component="h2">
-                {title}
+    // const toggleExpandedText = (id) => {
+    //     setExpandedText(prevState => ({ ...prevState, [id]: !prevState[id] }));
+    // };
+    const [open, setOpen] = useState(false);
+    const [modalContent, setModalContent] = useState('');
+    const [modalTitle, setModalTitle] = useState('');
+
+    const handleOpen = (title, text) => {
+        setModalTitle(title);
+        setModalContent(text);
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const body = (
+        <Box
+            sx={{
+                position: 'absolute',
+                top: '50%',
+                left: '50%',
+                transform: 'translate(-50%, -50%)',
+                width: 400,
+                bgcolor: 'background.paper',
+                border: '2px solid #000',
+                boxShadow: 24,
+                p: 4,
+            }}
+        >
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+                {modalTitle}
             </Typography>
-            <Typography color="textSecondary">
-                {children}
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                {modalContent}
             </Typography>
-        </CardContent>
-    </Card>
-);
-
-TimelineItem.propTypes = {
-    children: PropTypes.any,
-    title: PropTypes.any
-}
-
-const Timeline = ({ events }) => {
-    const [isDragging, setIsDragging] = useState(false);
-    const [startX, setStartX] = useState(0);
-    const [scrollLeft, setScrollLeft] = useState(0);
-    const timelineRef = useRef();
-
-    const handleMouseDown = (e) => {
-        setIsDragging(true);
-        setStartX(e.pageX - timelineRef.current.offsetLeft);
-        setScrollLeft(timelineRef.current.scrollLeft);
-    };
-
-    const handleMouseLeave = () => {
-        setIsDragging(false);
-    };
-
-    const handleMouseUp = () => {
-        setIsDragging(false);
-    };
-
-    const handleMouseMove = (e) => {
-        if (!isDragging) return;
-        e.preventDefault();
-        const x = e.pageX - timelineRef.current.offsetLeft;
-        const walk = (x - startX) * 3; // scroll-fast
-        timelineRef.current.scrollLeft = scrollLeft - walk;
-    };
+        </Box>
+    );
 
     return (
-        <div
-            style={{ ...timelineStyle, '&::-webkit-scrollbar': { display: 'none' } }} // For Chrome, Safari and Opera
-            onMouseDown={handleMouseDown}
-            onMouseLeave={handleMouseLeave}
-            onMouseUp={handleMouseUp}
-            onMouseMove={handleMouseMove}
-            ref={timelineRef}
-        >
-            {events.map((event, index) => (
-                <TimelineItem key={index} title={event.title}>
-                    {event.description}
-                </TimelineItem>
-            ))}
-        </div>
+        <Grid container spacing={3}>
+            {contenttop.map((data) => {
+                const titleWithoutBraces = data.title.replace(/ *\([^)]*\) */g, " ");
+                return (
+                    <Grid item xs={12} sm={6} md={4} key={data.id}>
+                        <Box
+                            sx={{
+                                padding: 2,
+                                marginBottom: 2,
+                                backgroundColor: 'grey.200',
+                            }}
+                        >
+                            <Typography variant="h6" sx={{ fontWeight: 'bold' }}>
+                                {data.year}
+                            </Typography>
+                            <Typography variant="body1" sx={{ marginTop: 1 }}>
+                                {titleWithoutBraces}
+                            </Typography>
+                            <Typography variant="body2" sx={{ marginTop: 1, fontWeight: 'bold', cursor: 'pointer' }} onClick={() => handleOpen(data.title, data.text)}>
+                                Details...
+                            </Typography>
+                        </Box>
+                    </Grid>
+                );
+            })}
+            <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                {body}
+            </Modal>
+        </Grid>
     );
-};
-
-Timeline.propTypes = {
-    events: PropTypes.shape({
-        map: PropTypes.func
-    })
 }
 
-export default Timeline;
+export default TimelineCard;
